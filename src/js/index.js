0f7f2195b31fa095a84bb8641b9728121b3d71f0
Dimitry Voishev Junior <123220620+Dimitry-Voishev-Junior@users.noreply.github.com>
2023-01-22 10:52:45 -0300

    Projeto Mario

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+
+const modal = document.querySelector(".modal");
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+
+const video = document.getElementById("video");
+
+const linkVideo = video.src;
+
+botaoTrailer.addEventListener("click", () => {
+    modal.classList.add("aberto");
+    video.setAttribute("src", linkVideo);
+    
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.remove("aberto")
+    video.setAttribute("src", "" )
+});
